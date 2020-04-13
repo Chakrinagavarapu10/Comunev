8ed0746c703a891c61672665997ea83b4cd1f4cb
Chakri <chakri.nagavarapu@theroboticshomepage.com>
2020-04-13 17:01:24 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {FormGroup,FormControl, Validators} from '@angular/forms';
+import { format } from 'url';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CommunevAssign';
+  toShow=true;
+
+  profile=new FormGroup(
+    {
+      // name :new FormControl('',[Validators.required,Validators.minLength(3),Validators.pattern(/^[a-zA-Z0-9_-]*$/)]),
+      fname:new FormControl('',[Validators.required]),
+      lname:new FormControl('',[Validators.required]),
+      uname:new FormControl('',[Validators.required]),
+      password:new FormControl('',[Validators.required]),
+      country:new FormControl('',[Validators.required]),
+      dob:new FormControl('',[Validators.required]),
+}
+  );
+
+  verify(event){
+    this.toShow=!this.toShow;
+    console.log(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { MatInputModule, MatFormFieldModule, MatIconModule, MatDialogModule, MatDatepickerModule, MatNativeDateModule } from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatDialogModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatNativeDateModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
